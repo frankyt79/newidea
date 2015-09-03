95fd29cdb6e43ea22c5503bf6a472acb14bd514f
unknown <terrayf@D03644.avelo.local>
2015-09-03 08:49:24 +0100

    int

---
+++ app.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+	angular
+		.module('app', [])
+		.controller('HomeController', ControllerController);
+
+	ControllerController.$inject = ['$log'];
+	function ControllerController(dependency1) {
+		var vm = this;
+		vm.title = "Hello World";
+
+		activate();
+
+		////////////////
+
+		function activate() { }
+	}
+})();
